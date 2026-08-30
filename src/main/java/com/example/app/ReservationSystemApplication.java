523d3ee93c8e750b57b6e5a34a3d13787ed7cb88
Yeaoke <Liare1@mail.ru>
2026-08-30 05:02:13 +0300

    settings

--- src/main/java/com/example/app/ReservationSystemApplication.java
+++ src/main/java/com/example/app/ReservationSystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class mainApplication {
+public class ReservationSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(mainApplication.class, args);
+		SpringApplication.run(ReservationSystemApplication.class, args);
 	}
 
 }
