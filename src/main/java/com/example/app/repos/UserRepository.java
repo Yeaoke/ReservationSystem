dbb313b50c1b93702e922f3b787b8e49b82e2f56
Yeaoke <Liare1@mail.ru>
2026-09-03 19:35:50 +0300

    UserService

--- src/main/java/com/example/app/repos/UserRepository.java
+++ src/main/java/com/example/app/repos/UserRepository.java
@@ -1,9 +1,19 @@
 package com.example.app.repos;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.app.models.User;
 
-public interface UserRepository extends JpaRepository<User, UUID> {}
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    @Query("""
+        SELECT DISTINCT r.user
+        FROM Reservation r
+        JOIN Review rev ON rev.reservation = r
+    """)
+    List<User> findAllUsersWhoMadeReview();
+}
--- src/main/java/com/example/app/services/UserService.java
+++ src/main/java/com/example/app/services/UserService.java
@@ -5,25 +5,30 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.app.dto.input.CreateUserDTO;
 import com.example.app.models.User;
 import com.example.app.repos.UserRepository;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @Service
+@RequiredArgsConstructor
 public class UserService {
-    
-    @Autowired
-    private UserRepository userRepository;
 
-    public User createUser(
-        CreateUserDTO dto
-    ) {
+    private final UserRepository userRepository;
+
+    public User createUser(CreateUserDTO dto) {
+
+        log.info(
+                "Creating user: email={}, thread={}",
+                dto.email(),
+                Thread.currentThread().getName()
+        );
+
         User user = new User();
 
         user.setId(UUID.randomUUID());
@@ -33,15 +38,37 @@ public class UserService {
         user.setPassword(dto.password());
         user.setPhoneNumber(dto.phoneNumber());
 
-        return userRepository.save(user);
+        User savedUser = userRepository.save(user);
+
+        log.info(
+                "User created: id={}",
+                savedUser.getId()
+        );
+
+        return savedUser;
     }
 
     public Optional<User> getUserById(UUID id) {
+
+        log.info(
+                "Getting user: id={}",
+                id
+        );
+
         return userRepository.findById(id);
     }
 
-    public List<User> getAllUsersWhoMadeReview() {
-        // норм логика
+    public List<User> getAllUsers() {
+
+        log.info("Getting all users");
+
         return userRepository.findAll();
+    }
+
+    public List<User> getAllUsersWhoMadeReview() {
+
+        log.info("Getting users who made reviews");
+
+        return userRepository.findAllUsersWhoMadeReview();
     }
 }
